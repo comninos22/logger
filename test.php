ad63d1fcd50310a0e4c6f02304598d3322c66d38
comninos22 <51789730+comninos22@users.noreply.github.com>
2022-03-10 11:01:34 +0200

    s

--- test.php
+++ test.php
@@ -8,4 +8,4 @@ var_dump(class_exists("\Composer\Autoload\ClassLoader"));
 $logger = new GeneralLogger("txt", 'test2');
 $logger->log(['123123', '12412', true, -2], "TEST");
 ////
-//
+//AAAAAAAAAAAAAAAAAAAAAAA
